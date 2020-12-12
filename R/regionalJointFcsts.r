43f2efc2dd5608482f55ad7726a9ad25cd311be9
w-hardy <51114571+w-hardy@users.noreply.github.com>
2020-12-12 23:19:45 +0000

    Reduced size of object returned by regionalJointFcsts and plan() calls within function

--- R/regionalJointFcsts.r
+++ R/regionalJointFcsts.r
@@ -1,10 +1,9 @@
 #' @title Forecasts for each regional unit
 #'
-#' @description Various fable and prophet forecasts for monthly quantity for each regional_unit.
+#' @description Various fable and prophet forecasts for monthly quantity for each regional_unit. Can make use of plan() futures.
 #'
 #' @param data A dataframe with time as `datename` and quantity as `n`.
 #' @param h The number of months that should forecast.
-#' @param keep_fits Logical. Should the model objects be kept. Keeping them will increase memory requirements, use with caution.
 #'
 #' @importFrom distributional variance
 #' @import dplyr
@@ -14,49 +13,31 @@
 #' @importFrom stats median quantile
 #' @import tsibble
 #'
-#' @return List of model fits and forecasts
+#' @return Model forecasts
 #' @export
 #'
 #' @examples
 #' \dontrun{regionalJointFcsts(data)}
 #'
-regionalJointFcsts <- function(data, h ="5 months", keep_fits = FALSE){
-  if(!is.logical(keep_fits)){stop("`keep_fits` must be of type logical")}
+regionalJointFcsts <- function(data, h ="5 months"){
   regions <- unique(data$regional_unit)
-
-  plan(multisession, gc = TRUE)
-
-  fable_fit <-
-    future_map_dfr(.x = regions,
-                   .f = ~fableModels(filter(data, regional_unit ==.x)),
-                   .options = furrr_options(seed = TRUE))
-  message("fable fits complete, step 1/4")
 
   fable_fcst <-
     future_map_dfr(.x = regions,
-                   .f = ~as_tibble(forecast(filter(fable_fit,
-                                                   regional_unit ==.x),
-                                            h = h)),
+                   .f = ~fableModels(filter(data, regional_unit ==.x)) %>%
+                     forecast(h = h) %>% as_tibble(),
                    .options = furrr_options(seed = TRUE))
-  message("fable forecasts complete, step 2/4")
-
-  prophet_fit <-
-    future_map_dfr(.x = regions,
-                   .f = ~prophetModels(filter(data, regional_unit ==.x)),
-                   .options = furrr_options(seed = TRUE))
-  message("prophet fits complete, step 3/4")
+  message("fable forecasts complete, step 1/2")
 
   prophet_fcst <-
     future_map_dfr(.x = regions,
-                   .f = ~as_tibble(forecast(filter(prophet_fit,
-                                                   regional_unit ==.x),
-                                            h = h)),
+                   .f = ~prophetModels(filter(data, regional_unit ==.x)) %>%
+                     forecast(h = h) %>% as_tibble(),
                    .options = furrr_options(seed = TRUE))
   # Some prophet forecasts are giving daily forecasts. We want monthly forecasts.
   # This may also be influencing accuracy
 
-  message("prophet forecasts complete, step 4/4")
-  plan(sequential)
+  message("prophet forecasts complete, step 2/2")
   # prophetModels now being run in parallel. Needed to reinstall prophet from
   # source
 
@@ -71,13 +52,5 @@ regionalJointFcsts <- function(data, h ="5 months", keep_fits = FALSE){
                   dist = n,
                   n = NULL)
 
-  if (keep_fits == TRUE) {
-    joint_fit <-
-      dplyr::left_join(fable_fit, prophet_fit, by = "regional_unit")
-
-    return(list(fits = joint_fit, forecasts = joint_fcst))
-  } else {
-    return(list(forecasts = joint_fcst))
-  }
-
+  return(joint_fcst)
 }
