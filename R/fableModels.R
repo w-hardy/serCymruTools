c5fd3d1c3309b0057d92d3a55a655dc242e19efc
w-hardy <51114571+w-hardy@users.noreply.github.com>
2021-01-18 14:40:42 +0000

    Altered models

--- R/fableModels.R
+++ R/fableModels.R
@@ -17,33 +17,32 @@
 #' \dontrun{fableModels(data)}
 
 fableModels <-
-function(data){
-  if(!is_tsibble(data)){data <- as_tsibble(data, index = datename)}
+  function(data){
 
-  value <- enquo(value)
+    if(!is_tsibble(data)){data <- as_tsibble(data, index = datename)}
 
-  data %>%
-    mutate(datename = yearmonth(datename)) %>%
-    model(trend_model1 = TSLM(n ~ trend()),#time series linear model
-          trend_model2 = TSLM(n ~ trend() + season("year")),
-          ets1 = ETS(n ~ trend()),
-          ets2 = ETS(n ~ trend() + season("A")),
-          ets3 = ETS(n ~ trend() + season("M")),
-          arima = ARIMA(n, stepwise = FALSE, approximation = FALSE),
-          #neur_net = NNETAR(n),
-          #fasster = FASSTER(n ~ season("1 year") + trend(1) + fourier(12)),
-          comb1 = combination_model(TSLM(n ~ trend()),
-                                    ETS(n ~ trend())),
-          comb2 = combination_model(TSLM(n ~ trend() + season("year")),
-                                    ETS(n ~ trend() + season("A"))),
-          stl_dcmp1 = decomposition_model(STL(n ~ trend(),
-                                              iterations = 1000,
-                                              robust = TRUE),
-                                          SNAIVE(season_adjust)),
-          stl_dcmp2 = decomposition_model(STL(n ~ trend() + season(),
-                                              iterations = 1000,
-                                              robust = TRUE),
-                                          SNAIVE(season_adjust)),
-          s_naive = SNAIVE(n),
-          .safely = TRUE)
-}
+    data %>%
+      mutate(datename = yearmonth(datename)) %>%
+      model(#trend_model1 = TSLM(n ~ trend()),#time series linear model
+            trend_model2 = TSLM(n ~ trend() + season("1 year")),
+            #ets1 = ETS(n ~ trend()),
+            ets2 = ETS(n ~ trend("A") + season("A")), # Holt-Winters Additive Model
+            #ets3 = ETS(n ~ trend() + season("M")),
+            arima = ARIMA(n, ic = "RMSEA", stepwise = FALSE),
+            #neur_net = NNETAR(n),
+            #fasster = FASSTER(n ~ season("1 year") + trend(1) + fourier(12)),
+            comb1 = combination_model(TSLM(n ~ trend()),
+                                      ETS(n ~ trend())),
+            comb2 = combination_model(TSLM(n ~ trend() + season("1 year")),
+                                      ETS(n ~ trend() + season("A"))),
+            # stl_dcmp1 = decomposition_model(STL(n ~ trend(),
+            #                                    # iterations = 1000,
+            #                                     robust = TRUE),
+            #                                 SNAIVE(season_adjust)),
+            stl_dcmp2 = decomposition_model(STL(n ~ trend() + season("1 year"),
+                                               # iterations = 1000,
+                                                robust = TRUE),
+                                            SNAIVE(season_adjust)),
+            s_naive = SNAIVE(n),
+            .safely = TRUE)
+  }
