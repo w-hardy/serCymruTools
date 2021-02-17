43cb241e8658f45175f69b83d41401f7b2480feb
w-hardy <51114571+w-hardy@users.noreply.github.com>
2021-02-17 17:10:42 +0000

    log transform n to keep all values >=0

--- R/prophetModels.r
+++ R/prophetModels.r
@@ -33,31 +33,31 @@ prophetModels <-
                        upper_window = 1))
 
     oc_prophet_model1 <-
-      fable.prophet::prophet(n ~ growth("linear", n_changepoints = 0) +
+      fable.prophet::prophet(log(n) ~ growth("linear", n_changepoints = 0) +
                                season("year", type = "additive") +
                                holiday(holidays))
     # oc_prophet_model2 <-
-    #   fable.prophet::prophet(n ~ growth("linear", n_changepoints = 0) +
+    #   fable.prophet::prophet(log(n) ~ growth("linear", n_changepoints = 0) +
     #                            season("year", type = "multiplicative") +
     #                            holiday(holidays))
     oc_prophet_model3 <-
-      fable.prophet::prophet(n ~ growth("linear") +
+      fable.prophet::prophet(log(n) ~ growth("linear") +
                                season("year", type = "additive") +
                                holiday(holidays))
     # oc_prophet_model4 <-
-    #   fable.prophet::prophet(n ~ growth("linear") +
+    #   fable.prophet::prophet(log(n) ~ growth("linear") +
     #                            season("year", type = "multiplicative") +
     #                            holiday(holidays))
     oc_prophet_model5 <-
-      fable.prophet::prophet(n ~ season("year", type = "additive") +
+      fable.prophet::prophet(log(n) ~ season("year", type = "additive") +
                                holiday(holidays))
     # oc_prophet_model6 <-
-    #   fable.prophet::prophet(n ~ season("year", type = "multiplicative") +
+    #   fable.prophet::prophet(log(n) ~ season("year", type = "multiplicative") +
     #                            holiday(holidays))
     oc_prophet_model7 <-
-      fable.prophet::prophet(n ~ holiday(holidays))
+      fable.prophet::prophet(log(n) ~ holiday(holidays))
     oc_prophet_model8 <-
-      fable.prophet::prophet(n)
+      fable.prophet::prophet(log(n))
 
     data %>%
       mutate(datename = yearmonth(datename)) %>%
