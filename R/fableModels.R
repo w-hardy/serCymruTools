593a01fce0b1cb58062da6f107e6dab707d07560
w-hardy <51114571+w-hardy@users.noreply.github.com>
2021-03-04 15:32:33 +0000

    Removed incorrect combination model and reincluded other models

--- R/fableModels.R
+++ R/fableModels.R
@@ -24,28 +24,28 @@ fableModels <-
 
     data %>%
       mutate(datename = yearmonth(datename)) %>%
-      model(#trend_model1 = TSLM(log(n) ~ trend()),#time series linear model
+      model(trend_model1 = TSLM(log(n) ~ trend()),#time series linear model
             trend_model2 = TSLM(log(n) ~ trend() + season("1 year")),
-            #ets1 = ETS(log(n) ~ trend()),
+            ets1 = ETS(log(n) ~ trend()),
             ets2 = ETS(log(n) ~ trend("A") + season("A")), # Holt-Winters Additive Model
-            #ets3 = ETS(log(n) ~ trend() + season("M")),
             arima = ARIMA(log(n), stepwise = FALSE),
-            #neur_net = NNETAR(log(n)),
             fasster = FASSTER(log(n) ~ season("1 year") + trend(1) + fourier(12)),
-            comb1 = combination_model(TSLM(log(n) ~ trend()),
-                                      ETS(log(n) ~ trend())),
-            comb2 = combination_model(TSLM(log(n) ~ trend() + season("1 year")),
-                                      ETS(log(n) ~ trend() + season("A"))),
-            # stl_dcmp1 = decomposition_model(STL(log(n) ~ trend(),
-            #                                    # iterations = 1000,
-            #                                     robust = TRUE),
-            #                                 SNAIVE(season_adjust)),
+            stl_dcmp1 = decomposition_model(STL(log(n) ~ trend(),
+                                                robust = TRUE),
+                                            SNAIVE(season_adjust)),
             stl_dcmp2 = decomposition_model(STL(log(n) ~ trend() + season("1 year"),
-                                               # iterations = 1000,
                                                 robust = TRUE),
                                             SNAIVE(season_adjust)),
             s_naive = SNAIVE(log(n)),
             s_naive_drift = SNAIVE(log(n) ~ drift()),
-            .safely = TRUE) %>%
-      mutate(comb3 = (arima + stl_dcmp2) / 2)
+            comb1 = combination_model(TSLM(log(n) ~ trend()),
+                                      ETS(log(n) ~ trend())),
+            comb2 = combination_model(TSLM(log(n) ~ trend() + season("1 year")),
+                                      ETS(log(n) ~ trend() + season("A"))),
+            comb3 = combination_model(ARIMA(log(n), stepwise = FALSE),
+                                      decomposition_model(STL(log(n) ~ trend() +
+                                                                season("1 year"),
+                                                              robust = TRUE),
+                                                          SNAIVE(season_adjust))),
+            .safely = TRUE)
   }
