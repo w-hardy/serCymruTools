99ab5668611a38f5805a9f16fd63c93e38661906
w-hardy <51114571+w-hardy@users.noreply.github.com>
2021-03-05 10:38:24 +0000

    Editing models

--- R/fableModels.R
+++ R/fableModels.R
@@ -26,15 +26,16 @@ fableModels <-
       mutate(datename = yearmonth(datename)) %>%
       model(trend_model1 = TSLM(log(n) ~ trend()),#time series linear model
             trend_model2 = TSLM(log(n) ~ trend() + season("1 year")),
-            ets1 = ETS(log(n) ~ trend()),
+            ets1 = ETS(log(n) ~ trend("N") + season("N")), # Simple exponential smoothing
             ets2 = ETS(log(n) ~ trend("A") + season("A")), # Holt-Winters Additive Model
-            arima = ARIMA(log(n), stepwise = FALSE),
+            arima_step = ARIMA(log(n)),
+            arima_search = ARIMA(log(n), stepwise = FALSE),
             fasster = FASSTER(log(n) ~ season("1 year") + trend(1) + fourier(12)),
             stl_dcmp1 = decomposition_model(STL(log(n) ~ trend(window = 21),
                                                 robust = TRUE),
                                             NAIVE(season_adjust)),
             stl_dcmp2 = decomposition_model(STL(log(n) ~ trend(window = 21) +
-                                                  season(window = "periodic"),
+                                                  season(window = "periodic"), # Fixed seasonality
                                                 robust = TRUE),
                                             NAIVE(season_adjust)),
             naive = NAIVE(log(n)),
@@ -42,12 +43,11 @@ fableModels <-
             s_naive = SNAIVE(log(n) ~ lag("year")),
             s_naive_drift = SNAIVE(log(n) ~ lag("year") + drift()),
             comb1 = combination_model(TSLM(log(n) ~ trend()),
-                                      ETS(log(n) ~ trend())),
+                                      ETS(log(n) ~ trend("N") + season("N"))),
             comb2 = combination_model(TSLM(log(n) ~ trend() + season("1 year")),
-                                      ETS(log(n) ~ trend() + season("A"))),
+                                      ETS(log(n) ~ trend("A") + season("A"))),
             comb3 = combination_model(ARIMA(log(n), stepwise = FALSE),
-                                      decomposition_model(STL(log(n) ~ trend() +
-                                                                season("1 year"),
+                                      decomposition_model(STL(log(n) ~ trend(window = 21),
                                                               robust = TRUE),
                                                           SNAIVE(season_adjust))),
             .safely = TRUE)
