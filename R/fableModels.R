890a6ad138a1cf250f3ae1c02779bf5848a2befb
w-hardy <51114571+w-hardy@users.noreply.github.com>
2021-03-05 10:42:10 +0000

    Typos

--- R/fableModels.R
+++ R/fableModels.R
@@ -28,8 +28,8 @@ fableModels <-
             trend_model2 = TSLM(log(n) ~ trend() + season("1 year")),
             ets1 = ETS(log(n) ~ trend("N") + season("N")), # Simple exponential smoothing
             ets2 = ETS(log(n) ~ trend("A") + season("A")), # Holt-Winters Additive Model
-            arima_step = ARIMA(log(n)),
-            arima_search = ARIMA(log(n), stepwise = FALSE),
+            arima_step = ARIMA(log(n)), # Default stepwise method
+            arima_search = ARIMA(log(n), stepwise = FALSE), # Search larger space
             fasster = FASSTER(log(n) ~ season("1 year") + trend(1) + fourier(12)),
             stl_dcmp1 = decomposition_model(STL(log(n) ~ trend(window = 21),
                                                 robust = TRUE),
